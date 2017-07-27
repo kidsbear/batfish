8cc0dcde1687a9ead84f78fbdc57fc4cab83c642
Daniel Halperin <dan@intentionet.com>
2017-07-27 14:18:11 -0700

    Put in the seeds of a RESTful API based on Jersey and Jackson
    Two example calls, to get information about all containers or a single
    container.
    
    Also adds a default exception mapper to return stack traces on failures.

--- projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgrService.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgrService.java
@@ -32,8 +32,6 @@ import org.codehaus.jettison.json.JSONObject;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /** */
-
-/** */
 @Path(CoordConsts.SVC_CFG_WORK_MGR)
 public class WorkMgrService {
 
